24adfdee13306f7c4832584e45ae4c75da0d14b3
DevGomes78 <gomes.amilson@gmail.com>
2022-03-09 17:29:48 -0300

    componentizado o text e botoes tratamento de erros try..catch.

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -4,6 +4,7 @@ import 'package:login_user/models/user_model.dart';
 
 class UserRepository {
   Future<List<Data>> GetUser() async {
+    try{
     final url = Uri.parse('https://reqres.in/api/users?page=2');
     List<Data> lista = [];
     final response = await http.get(url);
@@ -16,6 +17,9 @@ class UserRepository {
       );
       return lista;
     } else {
+      throw Exception('Erro ao acessar os dados');
+    }
+    }catch(e){
       throw Exception('Erro ao acessar os dados');
     }
   }
